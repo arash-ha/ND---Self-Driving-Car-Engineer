26aecde46f66cf18ffd757c75ca7291908b92b6d
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2019-10-09 12:13:58 -0400

    Update tools.cpp

--- Project - Extended Kalman Filter/src/tools.cpp
+++ Project - Extended Kalman Filter/src/tools.cpp
@@ -32,7 +32,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
    return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
   /**
    * TODO:
    * Calculate a Jacobian here.
